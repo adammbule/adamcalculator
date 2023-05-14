ffaabb7cdd742c6d70ad9ec7efdf419369e87e6d
adammbule <adammbule98@gmail.com>
2023-05-14 23:27:15 +0300

    Add files via upload
    Testing my understanding of functions

---
+++ main.dart
@@ -0,0 +1,42 @@
+import "dart:io";
+import "dart:math";
+
+//Two functions for taking the input and second for the operand
+double numInput(){
+  print("Enter the number to begin:");
+  double myNumber = double.parse(stdin.readLineSync()!);
+  return myNumber;
+
+}
+
+String chosenOperand(){
+  print("Enter the math operand e.g. '+', '-', '*', '/'");
+  String enteredOperand = stdin.readLineSync()!; // standard input
+  return enteredOperand;
+
+}
+
+void main(){
+  double number1 = numInput();
+  double number2 = numInput();
+  String choice = chosenOperand();
+  double answer = 0.0;
+
+  if (choice == '+'){
+    answer = number1 + number2;
+    print('$number1 $choice $number2 = $answer');
+  }else if (choice == '-'){
+    answer = number1 - number2;
+    print('$number1 $choice $number2 = $answer');
+  }else if(choice == '*'){
+    answer = number1 * number2;
+    print('$number1 $choice $number2 = $answer');
+  }else if(choice == '/'){
+    answer = number1/number2;
+    print('$number1 $choice $number2 = $answer');
+  }else{
+    print('Invalid Operand entered');
+  }
+  
+  
+}
